a756c8b718d31b0374d239c9bddd41c46f7390a1
jjerry-k <jaeyeol2931@gmail.com>
2022-09-19 00:31:27 +0900

    Update

---
+++ leetcode/2319.check-if-matrix-is-x-matrix.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=2319 lang=python3
+#
+# [2319] Check if Matrix Is X-Matrix
+# Your runtime beats 54.73 % of python3 submissions
+# Your memory usage beats 18.24 % of python3 submissions (15 MB)
+# @lc code=start
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        n = len(grid)
+        for i in range(n):
+            l_idx = i
+            r_idx = n-1-i
+            for j in range(n):
+                if (j in [l_idx, r_idx]) and (grid[i][j] == 0):
+                    return False
+                elif (j not in [l_idx, r_idx]) and (grid[i][j] != 0):
+                    return False
+        return True
+
+# @lc code=end
+
